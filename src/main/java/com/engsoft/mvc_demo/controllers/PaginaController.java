1381f629ce180f977311874f3a43bbbfafdd31f4
Marcella Ricoy <marcellaricoy@gmail.com>
2025-05-21 15:17:25 -0300

    feat: adicionando conteúdo inicial

--- src/main/java/com/engsoft/mvc_demo/controllers/PaginaController.java
+++ src/main/java/com/engsoft/mvc_demo/controllers/PaginaController.java
@@ -1,5 +1,29 @@
 package com.engsoft.mvc_demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.engsoft.mvc_demo.models.Musica;
+
+@Controller
+@RequestMapping("/musicas")
 public class PaginaController {
+
+    public List<Musica> musicas = new ArrayList<Musica>();
+
+    public PaginaController() {
+        musicas.add(new Musica("musica 1", "artista 1", 300));
+    }
+
+    @GetMapping
+    public String getMusicas(Model model) {
+        model.addAttribute("musicas", musicas);
+        return "musicas";
+    }
 
 }
--- src/main/java/com/engsoft/mvc_demo/models/Musica.java
+++ src/main/java/com/engsoft/mvc_demo/models/Musica.java
@@ -2,4 +2,37 @@ package com.engsoft.mvc_demo.models;
 
 public class Musica {
 
+    String titulo, artista;
+    int duracao;
+
+    public Musica(String artista, String titulo, int duracao) {
+        this.artista = artista;
+        this.duracao = duracao;
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
 }
